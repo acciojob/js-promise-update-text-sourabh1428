809031782bd6674c4cd426d22721ed07583ddba3
Sourabh Pathak <63462210+sourabh1428@users.noreply.github.com>
2023-10-26 15:48:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+const output=document.getElementById("output");
+const prom=new Promise((resolve,reject)=>{
+	setTimeOut(
+		resolve("Hello World");
+		,1000);
+});
+
+prom.then(function(data){output.innerHTML=data}):
